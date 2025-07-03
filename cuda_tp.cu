dde915cd520b196eccb1a5bd999865f9b0628187
Justin Green <jeg112@case.edu>
2025-07-03 15:51:22 -0400

    clang-format

--- cuda_tp.cu
+++ cuda_tp.cu
@@ -7,17 +7,10 @@
 #define MINIBATCH_MAX_SIZE NUM_THREADS
 
 __global__ void tensor_product_forward_kernel(
-  float* __restrict__ in1,
-  float* __restrict__ in2,
-  float* __restrict__ out,
-  uint16_t* __restrict__ out_indices,
-  float* __restrict__ cb_lut,
-  uint32_t* __restrict__ input_indices,
-  size_t len_indices,
-  size_t in1_size,
-  size_t in2_size,
-  size_t out_size,
-  size_t minibatch_size) {
+    float *__restrict__ in1, float *__restrict__ in2, float *__restrict__ out,
+    uint16_t *__restrict__ out_indices, float *__restrict__ cb_lut,
+    uint32_t *__restrict__ input_indices, size_t len_indices, size_t in1_size,
+    size_t in2_size, size_t out_size, size_t minibatch_size) {
   int curr_len_indices = (len_indices + NUM_BLOCKS - 1) / NUM_BLOCKS;
   int start_idx = curr_len_indices * blockIdx.x;
   int end_idx = start_idx + curr_len_indices;
@@ -33,9 +26,9 @@ __global__ void tensor_product_forward_kernel(
   int i = start_idx;
   int out_index = out_indices[i];
   for (start_idx; i < end_idx; i++) {
-    acc += in1[(input_indices[i] >> 10) & 0x3FF]
-        * in2[input_indices[i] & 0x3FF]
-        * cb_lut[(input_indices[i] >> 20) & 0x3FF];
+    acc += in1[(input_indices[i] >> 10) & 0x3FF] *
+           in2[input_indices[i] & 0x3FF] *
+           cb_lut[(input_indices[i] >> 20) & 0x3FF];
     if (input_indices[i] & 0x80000000) {
       out[out_index] += acc;
       acc = 0.0;
@@ -46,31 +39,15 @@ __global__ void tensor_product_forward_kernel(
 }
 
 void tensor_product_forward_cuda(
-  float* __restrict__ in1,
-  float* __restrict__ in2,
-  float* __restrict__ out,
-  uint16_t* __restrict__ out_indices,
-  float* __restrict__ cb_lut,
-  uint32_t* __restrict__ input_indices,
-  size_t len_indices,
-  size_t in1_size,
-  size_t in2_size,
-  size_t out_size,
-  int batch_size) {
+    float *__restrict__ in1, float *__restrict__ in2, float *__restrict__ out,
+    uint16_t *__restrict__ out_indices, float *__restrict__ cb_lut,
+    uint32_t *__restrict__ input_indices, size_t len_indices, size_t in1_size,
+    size_t in2_size, size_t out_size, int batch_size) {
   while (batch_size > 0) {
     int minibatch_size = batch_size < MINIBATCH_MAX_SIZE ? batch_size : MINIBATCH_MAX_SIZE;
     tensor_product_forward_kernel<<<NUM_BLOCKS, NUM_THREADS>>>(
-      in1,
-      in2,
-      out,
-      out_indices,
-      cb_lut,
-      input_indices,
-      len_indices,
-      in1_size,
-      in2_size,
-      out_size,
-      minibatch_size);
+        in1, in2, out, out_indices, cb_lut, input_indices, len_indices,
+        in1_size, in2_size, out_size, minibatch_size);
     batch_size -= MINIBATCH_MAX_SIZE;
     in1 += in1_size * MINIBATCH_MAX_SIZE;
     in2 += in2_size * MINIBATCH_MAX_SIZE;
