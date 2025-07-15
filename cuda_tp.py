46d45f7b74babc4abd2fcf70540c45ecf475d30e
Justin Green <electrojustin@gmail.com>
2025-07-15 09:08:45 -0400

    Add output irrep filtering and linear mixing support

--- cuda_tp.py
+++ cuda_tp.py
@@ -5,7 +5,7 @@ import random
 from torch.utils.cpp_extension import load
 
 class CudaTensorProduct(torch.nn.Module):
-  def __init__(self, irreps_in1, irreps_in2):
+  def __init__(self, irreps_in1, irreps_in2, irreps_out=None):
     assert(irreps_in1.dim <= 4096)
     assert(irreps_in2.dim <= 4096)
     assert(irreps_in1.lmax <= 15)
@@ -13,18 +13,37 @@ class CudaTensorProduct(torch.nn.Module):
     super().__init__()
     self.cuda_tp = load(name='cuda_tp', sources=['cuda_tp.cpp', 'cuda_tp.cu'])
     cb_matrix_layout = {}
-    cb_height = 0
+    self.cb_height = 0
     idx_in1 = 0
     for l1 in irreps_in1.ls:
       idx_in2 = 0
       for l2 in irreps_in2.ls:
         for l3 in range(abs(l1-l2), l1+l2+1):
+          if irreps_out is not None and l3 not in irreps_out.ls:
+            continue
           if l3 not in cb_matrix_layout:
             cb_matrix_layout[l3] = []
           cb_matrix_layout[l3].append((l1, l2, idx_in1, idx_in2))
-          cb_height += 2 * l3 + 1
+          self.cb_height += 2 * l3 + 1
         idx_in2 += 2 * l2 + 1
       idx_in1 += 2 * l1 + 1
+
+    self.flops = 0
+    if irreps_out is not None:
+      self.out_dims = irreps_out.dim
+      self.num_weights = 0
+      self.out_layout = {}
+      gather_offset = 0
+      self.gather_indices = []
+      for l in irreps_out.ls:
+        if l not in self.out_layout:
+          self.out_layout[l] = [0]
+        self.out_layout[l][0] += 1
+      for l in set(irreps_out.ls):
+        self.out_layout[l].append(len(cb_matrix_layout[l]))
+        self.num_weights += self.out_layout[l][0] * self.out_layout[l][1]
+        # 1 fused-multiply-add per weight in the dense matmul.
+        self.flops += (2 * l + 1) * self.out_layout[l][0] * self.out_layout[l][1]
 
     l3s = list(cb_matrix_layout.keys())
     l3s.sort()
@@ -33,22 +52,48 @@ class CudaTensorProduct(torch.nn.Module):
     in2_indices = []
     out_indices = []
     cb_vals = []
-    for l3 in l3s:
-      multiplicities = cb_matrix_layout[l3]
-      multiplicities.sort(key=lambda x: x[0] * irreps_in2.lmax + x[1])
-      for multiplicity in multiplicities:
-        l1, l2, in1_offset, in2_offset = multiplicity
-        cb_coeffs = o3.wigner_3j(l1, l2, l3)
+    if irreps_out is None:
+      for l3 in l3s:
+        multiplicities = cb_matrix_layout[l3]
+        multiplicities.sort(key=lambda x: x[0] * irreps_in2.lmax + x[1])
+        for multiplicity in multiplicities:
+          l1, l2, in1_offset, in2_offset = multiplicity
+          cb_coeffs = o3.wigner_3j(l1, l2, l3)
+          for m3 in range(0, 2 * l3 + 1):
+            for m2 in range(0, 2 * l2 + 1):
+              for m1 in range(0, 2 * l1 + 1):
+                if cb_coeffs[m1, m2, m3] == 0:
+                  continue
+                in1_indices.append(m1 + in1_offset)
+                in2_indices.append(m2 + in2_offset)
+                out_indices.append(m3 + row_offset)
+                cb_vals.append(float(cb_coeffs[m1, m2, m3] * math.sqrt(2 * l3 + 1)))
+                self.flops += 2 # 1 multiply and 1 fused-multiply-add.
+          row_offset += 2 * l3 + 1
+    else:
+      # If we need to do linear mixing at the end, it's more efficient to break
+      # the mixing up into dense matmul operations. In order to do this, we need
+      # to output m values in planes rather than the normal interleaved form.
+      row_offset = 0
+      for l3 in l3s:
+        multiplicities = cb_matrix_layout[l3]
+        multiplicities.sort(key=lambda x: x[0] * irreps_in2.lmax + x[1])
         for m3 in range(0, 2 * l3 + 1):
-          for m2 in range(0, 2 * l2 + 1):
-            for m1 in range(0, 2 * l1 + 1):
-              if cb_coeffs[m1, m2, m3] == 0:
-                continue
-              in1_indices.append(m1 + in1_offset)
-              in2_indices.append(m2 + in2_offset)
-              out_indices.append(m3 + row_offset)
-              cb_vals.append(float(cb_coeffs[m1, m2, m3] * math.sqrt(2 * l3 + 1)))
-        row_offset += 2 * l3 + 1
+          multiplicity_idx = 0
+          for multiplicity in multiplicities:
+            l1, l2, in1_offset, in2_offset = multiplicity
+            cb_coeffs = o3.wigner_3j(l1, l2, l3)
+            for m2 in range(0, 2 * l2 + 1):
+              for m1 in range(0, 2 * l1 + 1):
+                if cb_coeffs[m1, m2, m3] == 0:
+                  continue
+                in1_indices.append(m1 + in1_offset)
+                in2_indices.append(m2 + in2_offset)
+                out_indices.append(row_offset + m3 * len(multiplicities) + multiplicity_idx)
+                cb_vals.append(float(cb_coeffs[m1, m2, m3] * math.sqrt(2 * l3 + 1)))
+                self.flops += 2 # 1 multiply and 1 fused-multiply-add
+            multiplicity_idx += 1
+        row_offset += len(multiplicities) * (2 * l3 + 1)
 
     # Palette compression for the Clebsch-Gordon coefficients.
     self.cb_palette = list(set(cb_vals))
@@ -116,18 +161,47 @@ class CudaTensorProduct(torch.nn.Module):
     self.samples_per_block = 8
 
 
-  def forward(self, in1, in2):
+  def forward(self, in1, in2, weights=None):
     batch_size = in1.shape[0]
     padded_batch_size = self.samples_per_block * int((batch_size + self.samples_per_block - 1) / self.samples_per_block)
     padded_in1 = torch.zeros((padded_batch_size, in1.shape[1]))
     padded_in2 = torch.zeros((padded_batch_size, in2.shape[1]))
     padded_in1[:batch_size, :] = in1
     padded_in2[:batch_size, :] = in2
-    out = torch.zeros((padded_in1.shape[0], padded_in1.shape[1] * padded_in2.shape[1]))
-    return self.cuda_tp.forward(
+    intermediate = torch.zeros((padded_in1.shape[0], self.cb_height))
+    intermediate = self.cuda_tp.forward(
       padded_in1,
       padded_in2,
-      out,
+      intermediate,
       self.cb_palette,
       self.block_jobs,
       self.block_job_sizes)[:batch_size, :]
+    if weights is None:
+      return intermediate
+    else:
+      intermediate = torch.t(intermediate)
+      out = torch.zeros((batch_size, self.out_dims))
+      ls = list(self.out_layout.keys())
+      ls.sort()
+      intermediate_idx = 0
+      out_idx = 0
+      weights_idx = 0
+      gather_offset = 0
+      gather_indices = []
+      for l in ls:
+        intermediate_muls = self.out_layout[l][1]
+        out_muls = self.out_layout[l][0]
+        weights_len = intermediate_muls * out_muls
+        curr_weights = weights[weights_idx:weights_idx + weights_len].reshape((out_muls, intermediate_muls))
+        for m in range(0, 2 * l + 1):
+          out[:, out_idx:out_idx+out_muls] = \
+                  torch.t(torch.matmul(curr_weights, intermediate[intermediate_idx:intermediate_idx+intermediate_muls, :]))
+          intermediate_idx += intermediate_muls
+          out_idx += out_muls
+        # Create gather indices so we can re-interleave the m planes.
+        for i in range(0, out_muls):
+          for m in range(0, 2 * l + 1):
+            gather_indices.append(gather_offset + m * out_muls + i)
+        gather_offset += out_muls * (2 * l + 1)
+        weights_idx += weights_len
+      return out[:, gather_indices]
